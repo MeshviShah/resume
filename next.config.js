08bfe572fb05f3a78bb70cfa98e03cbe87f2e39e
Meshvi Shah <90126181+MeshviShah@users.noreply.github.com>
2025-06-20 16:22:30 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,12 @@
 /** @type {import('next').NextConfig} */
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-    experimental: {
-      serverActions: true,
-    },
-  
+  output: 'export', // ← Add this for static export
+  basePath: process.env.NODE_ENV === 'production' ? '/resume' : '',
+  experimental: {
+    serverActions: true,
+  },
 };
 
 module.exports = nextConfig;
