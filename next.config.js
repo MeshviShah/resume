7b4ff2e16539327503b4f2c68b4019ddd692f949
Meshvi Shah <90126181+MeshviShah@users.noreply.github.com>
2025-06-22 23:30:28 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,6 +2,11 @@
 const nextConfig = {
   reactStrictMode: true,
   output: 'export',
-};
+  distDir: 'out', // Explicitly set output directory
+  // Add these if you have images
+  images: {
+    unoptimized: true, // Required for static export
+  }
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
