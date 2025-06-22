6c22c4cfa307da5b664e9b358dd363ec9a3ed8d4
Meshvi Shah <90126181+MeshviShah@users.noreply.github.com>
2025-06-22 23:43:06 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
 module.exports = {
+  reactStrictMode: true,
   output: 'export',
+  distDir: 'out', // Explicitly set output directory
+  // Add these if you have images
   images: {
-    unoptimized: true // Required for static exports
-  },
+    unoptimized: true, // Required for static export
+  }
   // Add if deploying to project site (username.github.io/repo)
   basePath: '/resume',
   assetPrefix: '/resume/'
