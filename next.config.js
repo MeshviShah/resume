b7b90c2d0f9f6f225d17189ddff481da093d4df9
Meshvi Shah <90126181+MeshviShah@users.noreply.github.com>
2025-06-23 10:54:16 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@ module.exports = {
   reactStrictMode: true,
   output: 'export',
   distDir: 'out', // Explicitly set output directory
+  basePath: process.env.NODE_ENV === 'production' ? '/resume' : '',
   // Add these if you have images
   images: {
     unoptimized: true, // Required for static export
